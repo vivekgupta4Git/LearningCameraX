9c8eabf9080211a2af22a0624679cd13cabe1c3c
Vivek <itguru4all@gmail.com>
2022-01-26 01:00:41 +0530

    Cropping Image CameraX -Unsuccessful -Rotation & ViewPort Issue

--- app/src/main/java/com/vivek/learningproject/camerax/MainActivity.kt
+++ app/src/main/java/com/vivek/learningproject/camerax/MainActivity.kt
@@ -3,16 +3,21 @@ package com.vivek.learningproject.camerax
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.graphics.*
 import android.hardware.display.DisplayManager
+import android.media.Image
+import android.net.Uri
 import android.os.Build
 import android.util.DisplayMetrics
 import android.util.Log
+import android.util.Rational
 import android.util.Size
 import android.view.*
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -20,21 +25,26 @@ import java.util.concurrent.Executors
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.getSystemService
+import androidx.core.graphics.toRect
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.FileOutputStream
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
+
     private var imageCapture: ImageCapture? = null
-    private lateinit var holder : SurfaceHolder
+    private lateinit var mHolder : SurfaceHolder
     private lateinit var surfaceView : SurfaceView
     private lateinit var layout:ConstraintLayout
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
-
+    private lateinit var rectF : RectF
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,12 +52,6 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
 
         layout = findViewById(R.id.constraintLayout)
         // Request camera permissions
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
 
         // Set up the listener for take photo button
         camera_capture_button.setOnClickListener { takePhoto() }
@@ -58,18 +62,58 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
 
         surfaceView  = findViewById(R.id.overlay)
         surfaceView.setZOrderOnTop(true)
-        holder = surfaceView.holder
-        holder.setFormat(PixelFormat.TRANSPARENT)
-        holder.addCallback(this)
+        mHolder = surfaceView.holder
+
+        mHolder.setFormat(PixelFormat.TRANSPARENT)
+        mHolder.addCallback(this)
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onResume() {
+        super.onResume()
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
 
     }
 
     private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
 
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
 
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
     }
 
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("UnsafeOptInUsageError")
     private fun startCamera() {
         //using camera provider class, used to bind the lifecycle of cameras to the lifecycle owner.
         //this eliminates the task of opening and closing the camera since CameraX is lifecycle aweare.
@@ -77,27 +121,73 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
 
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+
+        cameraProviderFuture.addListener(Runnable {
+
+
+
+
+
+        },ContextCompat.getMainExecutor(this))
+
         cameraProviderFuture.addListener(Runnable {
         val bindLife = cameraProviderFuture.get()
 
+            //preview object
             val preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(previewView.surfaceProvider)
-
                 }
 
 
+            imageCapture = ImageCapture.Builder()
+                .build()
 
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.d(TAG, "Average luminosity: $luma")
+                    })
+                }
+
+
+            //camera selector
         val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            val widthOfCroppedImage =rectF.width().toInt()
+            val heightOfCroppedImage = rectF.height().toInt()
+            Log.i(TAG,"RectF : ${rectF.height()} ${rectF.width()}")
+            val rational = Rational(widthOfCroppedImage,heightOfCroppedImage)
+            Log.i(TAG,"Rational : $rational")
+            Log.i(TAG,"Rotation : ${display?.rotation}")
+
+            val viewPort =
+               display?.let { ViewPort.Builder(rational, it.rotation).build() }
+
+
+            val useCaseGroup = viewPort?.let {
+                UseCaseGroup.Builder()
+                    .addUseCase(preview)
+                    //    .addUseCase(imageAnalyzer)
+                    .addUseCase(imageCapture!!)
+                    .setViewPort(it)
+                    .build()
+            }
+
             try
             {
                 bindLife.unbindAll()
-                bindLife.bindToLifecycle(
-                    this,cameraSelector,preview
-                )
+                if (useCaseGroup != null) {
+                    bindLife.bindToLifecycle(
+                        this,cameraSelector,useCaseGroup)
+                }
+             //   bindLife.bindToLifecycle(this,
+              //  cameraSelector,preview,imageCapture,imageAnalyzer)
+
             }catch (e : Exception){
-                Log.e(TAG,"Use case binding fialed")
+                Log.e(TAG,"Use case binding failed")
             }
 
         },ContextCompat.getMainExecutor(this))
@@ -111,6 +201,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -164,6 +255,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
     }
 
     override fun surfaceCreated(p0: SurfaceHolder) {
+
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
@@ -174,26 +266,29 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
     }
 
     override fun surfaceRedrawNeeded(p0: SurfaceHolder) {
+
     }
 
     private fun drawOverlayRectangle(){
-        val displayMetrics = DisplayMetrics()
-        val defaultDisplay = getSystemService<DisplayManager>()?.getDisplay(Display.DEFAULT_DISPLAY)
-        defaultDisplay?.getMetrics(displayMetrics)
 
+      /*  val displayMetrics = DisplayMetrics()
+       val defaultDisplay= getSystemService(DisplayManager::class.java).getDisplay(Display.DEFAULT_DISPLAY)
+        defaultDisplay?.getMetrics(displayMetrics)
+*/
         val height = previewView.height
         val width = previewView.width
 
-        val canvas = holder.lockCanvas()
+
+        val canvas = mHolder.lockCanvas()
 
 
-        val left = height/2 + 10f
-        val top = height/2 + 10f
-        val right = width/2 + 10f
-        val bottom = width/2 + 10f
-
-        val rectF = RectF(left, top, right, bottom)
-
+        val left = width/4f
+        val top = height/4f
+        val right = width*3f/4f
+        val bottom = height/2f
+        Log.i(TAG,"in OnDraw, Surface View height x widht = $height x $width")
+        Log.i(TAG,"RectF, in OnDraw  Height X width =(Bottom-Top)x(Right-Left) : $bottom - $top x  $right - $left")
+         rectF = RectF(left, top, right, bottom)
         val paint = Paint().apply {
             Paint.ANTI_ALIAS_FLAG
             style=Paint.Style.STROKE
@@ -202,10 +297,30 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback2{
         }
 
         canvas.drawRect(rectF,paint)
-        holder.unlockCanvasAndPost(canvas)
+        mHolder.unlockCanvasAndPost(canvas)
     }
 
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
 
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
 
 }
 
